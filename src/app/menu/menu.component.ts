e4647423ece2c9956da7730fc3284f956b5ebd89
Chelitoo26 <alejocas890@gmail.com>
2023-03-22 12:34:52 -0600

    examen2

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Plato} from 'src/app/compartida/examen';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+}
